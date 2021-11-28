98a6e1601cce23f56d751b962a17ee92a2e18865
Koryagin Sergey <>
2021-11-28 22:10:11 +0200

    added new methods

--- ReadWriteSendApp/ReadWriteSendApp/ViewController.swift
+++ ReadWriteSendApp/ReadWriteSendApp/ViewController.swift
@@ -6,24 +6,39 @@
 //
 
 import UIKit
+import MessageUI
 
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
   
-  @IBOutlet weak var textTextField: UITextField!
-  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-  @IBOutlet weak var textView: UITextView!
+  @IBOutlet weak private var textTextField: UITextField!
+  @IBOutlet weak private var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet weak private var textView: UITextView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     self.textTextField.delegate = self
     activityIndicator.isHidden = true
-    
+    setupGestureRecognizers()
+  }
+  
+  @objc func tapGesture() {
+    view.endEditing(true)
   }
   
   @IBAction func writeInFile(_ sender: Any) {
     
+//    sendEmail()
+    
+    let str = "Super long string here"
+    let filename = getDocumentsDirectory().appendingPathComponent("output.txt")
+
+    do {
+      try str.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+    } catch {
+      print("FAILED!")
+      // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+    }
   }
   
   @IBAction func sendMessage(_ sender: Any) {
@@ -45,6 +60,27 @@ extension ViewController: UITextFieldDelegate {
   }
 }
 
+extension ViewController: MFMailComposeViewControllerDelegate {
+  func sendEmail() {
+    if MFMailComposeViewController.canSendMail() {
+      let mail = MFMailComposeViewController()
+      mail.mailComposeDelegate = self
+      mail.setToRecipients(["you@yoursite.com"])
+      mail.setMessageBody("<p>You're so awesome!</p>", isHTML: true)
+      
+      present(mail, animated: true)
+    } else {
+      print("failed send")
+      // show failure alert
+    }
+  }
+  
+  func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+    controller.dismiss(animated: true)
+  }
+}
+
+//MARK: - Private extension
 private extension ViewController {
   func readTextFromFile() {
     if let path = Bundle.main.path(forResource: "text", ofType: "txt") {
@@ -62,12 +98,12 @@ private extension ViewController {
     let modelPhone = UIDevice.current.model
     
     textView.text = """
-  Phone name: \(namePhone)
-  Phone model: \(modelPhone)
-  Battery level: \(batteryLevelPhone)
-  System name: \(systemNamePhone)
-  System version: \(systemVersionPhone)
-"""
+      Phone name: \(namePhone)
+      Phone model: \(modelPhone)
+      Battery level: \(batteryLevelPhone)
+      System name: \(systemNamePhone)
+      System version: \(systemVersionPhone)
+    """
   }
   
   func timeLapseForReading() {
@@ -90,6 +126,16 @@ private extension ViewController {
     activityIndicator.isHidden = false
     activityIndicator.color = UIColor.systemGray2
     activityIndicator.startAnimating()
+  }
+  
+  func setupGestureRecognizers() {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapGesture))
+    view.addGestureRecognizer(tapGesture)
+  }
+  
+  func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
   }
 }
 
